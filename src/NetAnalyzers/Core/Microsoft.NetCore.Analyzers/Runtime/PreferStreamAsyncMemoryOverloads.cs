d3be54d9c283a8a4eda57abf231f8a262b340a0b
carlossanlop <calope@microsoft.com>
2020-04-07 20:22:44 -0700

    Progress for fixer

--- src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferStreamAsyncMemoryOverloads.cs
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferStreamAsyncMemoryOverloads.cs
@@ -18,10 +18,20 @@ namespace Microsoft.NetCore.Analyzers.Runtime
     {
         internal const string RuleId = "CA1834";
 
-        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsTitle), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableTitle = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsTitle),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
 
-        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsMessage), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
-        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsDescription), MicrosoftNetCoreAnalyzersResources.ResourceManager, typeof(MicrosoftNetCoreAnalyzersResources));
+        private static readonly LocalizableString s_localizableMessage = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsMessage),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
+
+        private static readonly LocalizableString s_localizableDescription = new LocalizableResourceString(
+            nameof(MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsDescription),
+            MicrosoftNetCoreAnalyzersResources.ResourceManager,
+            typeof(MicrosoftNetCoreAnalyzersResources));
 
         internal static DiagnosticDescriptor PreferStreamAsyncMemoryOverloadsRule = DiagnosticDescriptorHelper.Create(
                                                                                         RuleId,
@@ -40,10 +50,23 @@ namespace Microsoft.NetCore.Analyzers.Runtime
             context.EnableConcurrentExecution();
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.RegisterOperationAction(
-                context => AnalyzeInvocationExpression(
-                    (IInvocationOperation)context.Operation,
-                    context.ReportDiagnostic),
+                context =>
+                {
+                    if (context.Operation is IInvocationOperation invocation &&
+                        IsUndesiredStreamWriteAsyncMethod(invocation))
+                    {
+                        context.ReportDiagnostic(invocation.Syntax.CreateDiagnostic(PreferStreamAsyncMemoryOverloadsRule));
+                    }
+                },
                 OperationKind.Invocation);
+        }
+
+        private static bool IsUndesiredStreamWriteAsyncMethod(IInvocationOperation invocation)
+        {
+            return invocation.Arguments.Length > 0 &&
+                invocation.TargetMethod is IMethodSymbol method &&
+                IsStreamWriteAsyncMethod(method) &&
+                HasUndesiredArguments(method);
         }
 
         private static bool IsStreamWriteAsyncMethod(IMethodSymbol method)
@@ -52,7 +75,7 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                 method.ContainingType.ToString() == WellKnownTypeNames.SystemIOStream;
         }
 
-        private static bool HasByteArrayArgument(IMethodSymbol method)
+        private static bool HasUndesiredArguments(IMethodSymbol method)
         {
             return method.Parameters.Length == 3 &&
                 method.Parameters[0].Type.TypeKind == TypeKind.Array &&
@@ -60,104 +83,5 @@ namespace Microsoft.NetCore.Analyzers.Runtime
                 method.Parameters[2].Type.SpecialType == SpecialType.System_Int32;
         }
 
-        private static void AnalyzeInvocationExpression(IInvocationOperation invocation
-            , Action<Diagnostic> reportDiagnostic)
-        {
-            if (invocation.Arguments.Length > 0)
-            {
-                IMethodSymbol methodSymbol = invocation.TargetMethod;
-                if (methodSymbol != null &&
-                    IsStreamWriteAsyncMethod(methodSymbol) &&
-                    HasByteArrayArgument(methodSymbol))
-                {
-                    reportDiagnostic(invocation.Syntax.CreateDiagnostic(PreferStreamAsyncMemoryOverloadsRule));
-                }
-            }
-        }
     }
 }
-
-
-
-
-
-/////
-//var memoryType = compilationContext.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemMemory);
-//if (memoryType == null)
-//{
-//    return;
-//}
-
-//var readOnlyMemoryType = context.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemReadOnlyMemory);
-//if (readOnlyMemoryType == null)
-//{
-//    return;
-//}
-
-//var streamType = context.Compilation.GetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemIOStream);
-//if (streamType == null)
-//{
-//    return;
-//}
-
-//var byteType = context.Compilation.GetSpecialType(SpecialType.System_Byte);
-//if (byteType == null)
-//{
-//    return;
-//}
-
-//Func<IMethodSymbol, bool> paramFilter = (m) => m.Parameters.Length == 4 && Equals(m.Parameters[0].Type, byteType) && m.Parameters[0].Kind == SymbolKind.ArrayType;
-
-////var readAsyncMethod = streamType.GetMembers("ReadAsync").OfType<IMethodSymbol>().FirstOrDefault(paramFilter);
-////if (readAsyncMethod == null)
-////{
-////    return;
-////}
-
-//var writeAsyncMethod = streamType.GetMembers("WriteAsync").OfType<IMethodSymbol>().FirstOrDefault(paramFilter);
-//if (writeAsyncMethod == null)
-//{
-//    return;
-//}
-
-//context.RegisterOperationAction(operationContext =>
-//{
-//    var invocation = (IInvocationOperation)operationContext.Operation;
-//    if (invocation.TargetMethod == null)
-//    {
-//        return;
-//    }
-
-//    var method = invocation.TargetMethod;
-//    //if (method.Equals(readAsyncMethod))
-//    //{
-
-//    //    var suggestedReadAsyncMethod = streamType.GetMembers("ReadAsync").OfType<IMethodSymbol>().FirstOrDefault(m => m.Parameters.Length == 2 && Equals(m.Parameters[0].Type, memoryType));
-
-//    //    if (suggestedReadAsyncMethod != null)
-//    //    {
-//    //        var diagnostic = Diagnostic.Create(
-//    //            descriptor: PreferStreamAsyncMemoryOverloadsRule,
-//    //            location: invocation.Syntax.GetLocation(),
-//    //            operationContext.ContainingSymbol.Name,
-//    //            method.Name,
-//    //            suggestedReadAsyncMethod.Name);
-//    //    }
-//    //}
-//    //else 
-//    if (method.Equals(writeAsyncMethod))
-//    {
-//        var suggestedWriteAsyncMethod = streamType.GetMembers("ReadAsync").OfType<IMethodSymbol>().FirstOrDefault(m => m.Parameters.Length == 2 && Equals(m.Parameters[0].Type, readOnlyMemoryType));
-
-//        if (suggestedWriteAsyncMethod != null)
-//        {
-//            var diagnostic = Diagnostic.Create(
-//                descriptor: PreferStreamAsyncMemoryOverloadsRule,
-//                location: invocation.Syntax.GetLocation(),
-//                operationContext.ContainingSymbol.Name,
-//                method.Name,
-//                suggestedWriteAsyncMethod.Name);
-//        }
-//    }
-
-//}, OperationKind.Invocation);
--- src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferStreamAsyncMemoryOverloadsFixer.cs
+++ src/NetAnalyzers/Core/Microsoft.NetCore.Analyzers/Runtime/PreferStreamAsyncMemoryOverloadsFixer.cs
@@ -1,10 +1,18 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using System.Composition;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Editing;
+using Microsoft.CodeAnalysis.Operations;
 
 namespace Microsoft.NetCore.Analyzers.Runtime
 {
@@ -16,15 +24,59 @@ namespace Microsoft.NetCore.Analyzers.Runtime
     {
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(PreferStreamAsyncMemoryOverloads.RuleId);
 
-        public override Task RegisterCodeFixesAsync(CodeFixContext context)
+        public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            return Task.CompletedTask;
+            Document doc = context.Document;
+            SyntaxNode root = await doc.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            if (root.FindNode(context.Span) is SyntaxNode expression)
+            {
+                string title = MicrosoftNetCoreAnalyzersResources.PreferStreamAsyncMemoryOverloadsTitle;
+
+                var action = new CodeActionInternal(title,
+                    async ct => await ConvertToPreferredWriteAsync(doc, expression, ct).ConfigureAwait(false),
+                    equivalenceKey: title);
+
+                context.RegisterCodeFix(action, context.Diagnostics);
+            }
         }
-        public sealed override FixAllProvider GetFixAllProvider()
+
+        private async Task<Document> ConvertToPreferredWriteAsync(Document document, SyntaxNode nodeToFix, CancellationToken cancellationToken)
         {
-            // For more information on Fix All Providers,
-            // see https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md
-            return WellKnownFixAllProviders.BatchFixer;
+            DocumentEditor editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
+            SemanticModel semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
+            SyntaxGenerator generator = editor.Generator;
+
+            if (semanticModel.GetOperation(nodeToFix, cancellationToken) is IInvocationOperation invocation &&
+                invocation.Arguments.Length > 0 &&
+                invocation.GetReceiverType(semanticModel.Compilation, beforeConversion: true, cancellationToken) is INamedTypeSymbol type)
+            {
+                IMethodSymbol? replacement = type.GetMembers("WriteAsync").OfType<IMethodSymbol>().FirstOrDefault(x => x.Parameters.Length == 2);
+
+                if (replacement == null)
+                {
+                    return document;
+                }
+
+                
+
+                SyntaxNode nodeReplacement;
+
+                editor.ReplaceNode(nodeToFix, nodeReplacement.WithTriviaFrom(nodeToFix));
+                return editor.GetChangedDocument();
+            }
+
+            return document;
+        }
+
+        private sealed class CodeActionInternal : DocumentChangeAction
+        {
+            public CodeActionInternal(string title, Func<CancellationToken, Task<Document>> createChangedDocument, string equivalenceKey) :
+                base(title, createChangedDocument, equivalenceKey)
+            {
+            }
         }
     }
 }
